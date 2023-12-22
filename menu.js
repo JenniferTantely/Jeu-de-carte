b8628b0b9dc3102dd68096127755af70f2564c3c
AndrainaHEI <154558898+AndrainaHEI@users.noreply.github.com>
2023-12-22 12:47:07 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,30 @@
+const prompt = require("prompt-sync")();
+/**
+ * @function menu
+ * Cette fonction affiche le menu du jeu
+ */
+function menu() {
+    console.log("");
+    console.log("                                                 <-------------------> ");
+    console.log("                                                 <                   >  ");
+    console.log("                                                 <   JEU DE CARTES   >    ");
+    console.log("                                                 <                   >      ");
+    console.log("                                                 <-------------------> ");
+    console.log("");
+    console.log("");
+    console.log("                                                    [ MENU DU JEU ] ");
+    console.log("");
+    console.log("                                                _______________________ ");
+    console.log("                                               |***********************| ");
+    console.log("                                               |  Nous vous souhaitons | ");
+    console.log("                                               |      la bienvenue     | ");
+    console.log("                                               |                       | ");
+    console.log("                                               |      1- JOUER         | ");
+    console.log("                                               |           ou          | ");
+    console.log("                                               |      2- TUTORIEL      | ");
+    console.log("                                               |           ou          | ");
+    console.log("                                               |      3- QUITTER       | ");
+    console.log("                                               |.......................| ");
+    console.log("                                               |_______________________| ");
+}
+menu()
